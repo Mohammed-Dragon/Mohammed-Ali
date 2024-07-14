2d46af34d6e41d4747039495f4ebb74b0ad2dabf
Mohammed-Dragon <alu3535dragon@gmail.com>
2024-07-14 08:00:05 +0300

    task 6

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       /*BlocProvider(
             create: (context) => FilterCubitCubit(),
             child: FastFilterBarUsingCubit(),
-          ),*/
+          ), */
     );
   }
 }
